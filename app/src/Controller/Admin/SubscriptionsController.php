d787f4e35c09267203ae1489e6b5797d830dd0d3
thnardi <thnardi@gmail.com>
2018-05-16 12:17:15 -0300

    export

--- app/src/Controller/Admin/SubscriptionsController.php
+++ app/src/Controller/Admin/SubscriptionsController.php
@@ -10,13 +10,10 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Flash\Messages as FlashMessages;
 use Slim\Views\Twig as View;
 
-use Fusonic\SpreadsheetExport\Spreadsheet;
-use Fusonic\SpreadsheetExport\ColumnTypes\DateColumn;
-use Fusonic\SpreadsheetExport\ColumnTypes\NumericColumn;
-use Fusonic\SpreadsheetExport\ColumnTypes\TextColumn;
-use Fusonic\SpreadsheetExport\Writers\CsvWriter;
-
-use Port\Csv\CsvReader;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Reader\IReader;
 
 use Farol360\Ancora\Model;
 use Farol360\Ancora\Model\EntityFactory;
@@ -197,29 +194,149 @@ class SubscriptionsController extends Controller
 
         $eventId = intval($args['id']);
 
+        $event = $this->eventModel->get($eventId);
+
         $subscriptions = $this->subscriptionModel->getAllByEvent($eventId);
 
-        $export = new Spreadsheet();
-        $export->addColumn(new TextColumn('Participante'));
-        $export->addColumn(new TextColumn('Email'));
-        $export->addColumn(new DateColumn('Data do cadastro'));
-        $export->addColumn(new TextColumn('Evento'));
-        $export->addColumn(new TextColumn('Carga Horária'));
+        $spreadsheet = new Spreadsheet();
 
+        $spreadsheet->getActiveSheet()->setCellValue('B1','LISTA DE PRESENÇA');
+        $spreadsheet->getActiveSheet()->mergeCells('B1:C1');
+        $spreadsheet->getActiveSheet()->setCellValue('D1','EVENTO: '. $event->name );
+        $spreadsheet->getActiveSheet()->mergeCells('D1:F1');
+        $spreadsheet->getActiveSheet()->setCellValue('A2','ID Inscrição');
+        $spreadsheet->getActiveSheet()->setCellValue('B2','Participante');
+        $spreadsheet->getActiveSheet()->setCellValue('C2','Email');
+        $spreadsheet->getActiveSheet()->setCellValue('D2','Data do cadastro');
+        $spreadsheet->getActiveSheet()->setCellValue('E2','Evento');
+        $spreadsheet->getActiveSheet()->setCellValue('F2','Carga Horária');
+
+        $i = 3;
         foreach ($subscriptions as $subscription) {
-            $export->addRow([
-                $subscription->user_name,
-                $subscription->user_email,
-                $subscription->created_at,
-                $subscription->event_name,
-                $subscription->workload,
+            $spreadsheet->getActiveSheet()->setCellValue("A$i",
+                $subscription->id );
+            $spreadsheet->getActiveSheet()->setCellValue("B$i",
+                $subscription->user_name );
 
-            ]);
+            $spreadsheet->getActiveSheet()->setCellValue("C$i",
+                $subscription->user_email );
+
+            $spreadsheet->getActiveSheet()->setCellValue("D$i",
+                $subscription->created_at );
+
+            $spreadsheet->getActiveSheet()->setCellValue("E$i",
+                $subscription->event_name );
+
+            $spreadsheet->getActiveSheet()->setCellValue("F$i",
+                $subscription->workload );
+
+            $i++;
         }
-        $writer = new CsvWriter();
-        $writer->includeColumnHeaders = true;
-        // TODO: Refatorar para usar PSR-7
-        $export->download($writer, 'Lista-Inscricoes-' . time());
+
+        //style sheet
+        $styleTitle = [
+            'font' => [
+                'bold' => true,
+                'size' => 18,
+            ],
+            'alignment' => [
+                'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT,
+                'vertical' => \PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER,
+            ],
+        ];
+
+        $spreadsheet->getActiveSheet()->getStyle('B1')->applyFromArray($styleTitle);
+
+        $styleEventName = [
+            'font' => [
+                'size' => 14,
+            ],
+            'alignment' => [
+                'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT,
+                'vertical' => \PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_TOP,
+            ],
+        ];
+
+        $spreadsheet->getActiveSheet()->getStyle('D1')->applyFromArray($styleEventName  );
+
+        $styleHeader = [
+            'fill' => [
+                'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                'startColor' => [
+                    'argb' => 'FDFDFD',
+                ]
+            ],
+        ];
+
+        $spreadsheet->getActiveSheet()->getStyle('A1:F1')->applyFromArray($styleHeader);
+
+        $styleArray = [
+            'font' => [
+                'bold' => true,
+                'size' => 13,
+            ],
+            'alignment' => [
+                'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+            ],
+
+            'fill' => [
+                'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                'startColor' => [
+                    'argb' => 'DDDDDD',
+                ]
+            ],
+        ];
+        $spreadsheet->getActiveSheet()->getStyle('A2:F2')->applyFromArray($styleArray);
+
+        $spreadsheet->getActiveSheet()->getProtection()->setSheet(true);
+
+        $lockedCells = [
+            'protection' => ['locked' => \PhpOffice\PhpSpreadsheet\Style\Protection::PROTECTION_PROTECTED],
+        ];
+
+        $unlockedCells = [
+            'protection' => ['locked' => \PhpOffice\PhpSpreadsheet\Style\Protection::PROTECTION_UNPROTECTED],
+        ];
+
+        $spreadsheet->getActiveSheet()->getStyle("A3:E$i")->applyFromArray($lockedCells);
+
+        $spreadsheet->getActiveSheet()->getStyle("F3:F$i")->applyFromArray($unlockedCells);
+
+        $spreadsheet->getActiveSheet()->getColumnDimension('A')->setAutoSize(true);
+        $spreadsheet->getActiveSheet()->getColumnDimension('B')->setAutoSize(true);
+        $spreadsheet->getActiveSheet()->getColumnDimension('C')->setAutoSize(true);
+        $spreadsheet->getActiveSheet()->getColumnDimension('D')->setAutoSize(true);
+        $spreadsheet->getActiveSheet()->getColumnDimension('E')->setAutoSize(true);
+        $spreadsheet->getActiveSheet()->getColumnDimension('F')->setAutoSize(true);
+
+        $spreadsheet->getActiveSheet()->getRowDimension('1')->setRowHeight(50);
+
+        //SET IMAGE
+
+        $drawing = new \PhpOffice\PhpSpreadsheet\Worksheet\Drawing();
+        $drawing->setName('Logo');
+        $drawing->setDescription('Logo');
+        $drawing->setPath('images/logo.png');
+        $drawing->setHeight(52);
+        $drawing->setCoordinates('A1');
+
+        $drawing->setWorksheet($spreadsheet->getActiveSheet());
+
+
+
+
+        $file_path = "upload/export/";
+        $file_name = $file_path . 'Lista-Inscricoes-' . time().'.xlsx';
+
+        $writer = new Xlsx($spreadsheet);
+        $writer->save($file_name);
+
+         $response = $response->withHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        $response = $response->withHeader('Content-Disposition', 'attachment; filename="file.xlsx"');
+
+        $stream = fopen($file_name, 'r+');
+
+        return $response->withBody(new \Slim\Http\Stream($stream));
     }
 
     public function import(Request $request, Response $response, array $args)
@@ -246,23 +363,34 @@ class SubscriptionsController extends Controller
                 // move img to path
                 $file->moveTo($path . $filename);
 
-                $file = new \SplFileObject($path . $filename);
 
-                $delimiter = ';';
-                $enclosure = '"';
-                $escape = '\\';
+                // spreadsheet logic
+                $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($path . $filename);
 
-                $reader = new CsvReader($file, $delimiter, $enclosure, $escape);
+               $sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
 
-                // Tell the reader that the first row in the CSV file contains column headers
-                $reader->setHeaderRowNumber(0);
 
-                foreach ($reader as $key => $value) {
-                    var_dump($key);
-                    var_dump($value);
-                }
+               $i = 1;
+               foreach ($sheetData as $key => $value) {
+                    if ($key > 1) {
+                        $subscription[$i]->id = $value['A'];
+                        $subscription[$i]->user_name = $value['B'];
+                        $subscription[$i]->user_email = $value['C'];
+                        $subscription[$i]->created_at = $value['D'];
+                        $subscription[$i]->event_name = $value['E'];
+                        $subscription[$i]->workload = $value['F'];
 
-                die;
+                        $i++;
+                        var_dump($subscription);
+                    }
+
+               }
+
+                var_dump($sheetData);
+
+                unlink($path . $filename);
+
+
 
             }
         }
